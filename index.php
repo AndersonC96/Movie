18f09b9ba0952cfde87d6db6ce7959f6a8b67351
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-12-18 17:09:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-    session_start();
-        if(!isset($_SESSION['username'])){
-            header('location: guest/index.php');
-        }elseif(isset($_SESSION['username'])=="admin"){
-            header('location: admin/index.php');
-        }else{
-            header('location: user/index.php');
+    session_start();// início da sessão
+        if(!isset($_SESSION['username'])){// se não existir a sessão
+            header('location: guest/index.php');// redireciona para a página de login
+        }elseif(isset($_SESSION['username'])=="admin"){// se existir a sessão e o valor for igual a admin
+            header('location: admin/index.php');// redireciona para a página de admin
+        }else{// se não for admin
+            header('location: user/index.php');// redireciona para a página de usuário
         }
 ?>
